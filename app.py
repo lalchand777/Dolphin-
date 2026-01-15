9589eaf62a0e4fbbb4b253db5248f8341f83648e
X <lalchandsuthar0973@gmail.com>
2026-01-15 16:00:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if prompt := st.chat_input("Puchiye, main seedha jawab dunga..."):
         
         try:
             stream = client.chat.completions.create(
-                model="cognitivecomputations/dolphin-llama-3-8b",
+                model="gryphe/mythomax-l2-13b",
                 messages=st.session_state.messages,
                 stream=True,
             )
